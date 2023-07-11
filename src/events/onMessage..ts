32abd12cec550d606bd154601dcfa94f030eee7c
memo12334 <mehmet1233445@gmail.com>
2023-07-11 18:20:25 +0200

    add clear command

--- src/events/onMessage..ts
+++ src/events/onMessage..ts
@@ -1,24 +1,52 @@
-import { Message } from 'discord.js';
+import { Message, PermissionFlagsBits, PermissionsBitField, TextChannel } from 'discord.js';
 import { getRoleByName } from '../db/repos/rolesRepo';
 
+const prefixes = ['!', '++', '.']
+
 export const onMessage = async (message: Message) => {
-  if (!message.content.startsWith('!')) return
+  const prefix = message.content.slice(0, 1)
+  if (!prefixes.some(p => p === prefix)) return
 
   const arg = message.content.slice(1).split(' ')[0]
-  const role = await getRoleByName(arg)
 
-  const userRole = message.member!.roles.cache.find(r => r.name === arg)
+  switch (prefix) {
+    case '!':
+      await handleRole(message, arg)
+      break
+    case '++':
+      break
+    case '.':
+      await purge(message, arg)
+      break
+  }
+}
 
-  if (role) {
-    if (userRole) {
-      if (message.member!.roles.cache.has(userRole.id)) {
-        message.member!.roles.remove(userRole)
+const purge = async (message: Message, arg: string) => {
+  if (!message.member?.permissions.has(PermissionsBitField.Flags.Administrator)) return
+
+  if (arg === 'clear') {
+    const fetched = await message.channel.messages.fetch({ limit: 50 })
+    const textchannel = message.channel as TextChannel
+    textchannel.bulkDelete(fetched)
+  }
+}
+
+const handleRole = async (message: Message, arg: string) => {
+  if (await getRoleByName(arg)) return
+
+  const { guild, member } = message
+
+  if (guild && member) {
+    const userRole = member.roles.cache.find(r => r.name === arg)
+    if (userRole) { // if user has role then remove
+      if (member.roles.cache.has(userRole.id)) {
+        member.roles.remove(userRole)
         message.reply(`You have been removed from the ${userRole.name} role.`)
       }
     }
     else {
-      const role = message.guild!.roles.cache.find(r => r.name === arg)
-      message.member!.roles.add(role!)
+      const role = guild.roles.cache.find(r => r.name === arg)
+      member.roles.add(role!)
       message.reply(`You have been added to the ${arg} role.`)
     }
   }
